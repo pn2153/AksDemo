0d1eb3626690bc9eb19dc685f8cdd9c35549d39b
Niyati Phanse <niyati.phanse8@gmail.com>
2026-05-17 09:38:23 +0000

    Deploy v4 via ArgoCD

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello from AKS + Helm + ArgoCD!'
+    return 'Version 2 deployed via Helm and ArgoCD!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
