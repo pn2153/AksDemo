df778b69af6fb86df860ece9919b9d2c468e117c
Niyati Phanse <niyati.phanse8@gmail.com>
2026-05-17 10:44:08 +0000

    Add ConfigMap support

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Version 2 deployed via Helm and ArgoCD!'
+    message = os.getenv("APP_MESSAGE", "Default Message")
+    return message
+
+@app.route('/health')
+def health():
+    return {
+        "status": "UP"
+    }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
